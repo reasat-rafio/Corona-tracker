0bdcc2fc7576cf3148c8b66a30e2612f60cfb7a4
Reasat RafiO <rafioar96@gmail.com>
2020-06-12 03:53:15 +0600

    Updates

---
+++ precache-manifest.94c23e248901f96c281cf17c628b9f82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a3bc1e801cd57dbd19318323ef2d33",
+    "url": "/Corona-tracker/index.html"
+  },
+  {
+    "revision": "92ddd1fb93acc33e3994",
+    "url": "/Corona-tracker/static/css/main.13066602.chunk.css"
+  },
+  {
+    "revision": "ccb747aa049b2a1868b2",
+    "url": "/Corona-tracker/static/js/2.6c805498.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Corona-tracker/static/js/2.6c805498.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92ddd1fb93acc33e3994",
+    "url": "/Corona-tracker/static/js/main.f3d3b0a9.chunk.js"
+  },
+  {
+    "revision": "e793a2ebc8b671d1faf9",
+    "url": "/Corona-tracker/static/js/runtime-main.e6020de3.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Corona-tracker/static/media/image.d7265326.png"
+  }
+]);
